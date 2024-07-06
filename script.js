5f17c79aa5d1b3edf426961f01606528f3550d24
abel <abelalebachewasfaw.gmail.com>
2024-07-06 12:47:15 +0300

    Add HTML, CSS and Javascript files

---
+++ script.js
@@ -0,0 +1,32 @@
+// const container = document.querySelector(".container");
+// const button1= document.getElementById("cell-0");
+// // let displayedContent = "";
+// // displayElement.textContent = displayedContent;
+// let xoState = "waitingForXo1";//initial state
+// let xo;
+// const xoButtons = document.querySelectorAll(".class");
+// xoButtons.forEach((button) =>{
+//   button.addEventListener("click", function(){
+//     if (xo === ""){
+//       displayedContent += button.getAttribute("value");
+//       updateDisplay(displayedContent);
+//       button.textContent="x";
+//       xo = "x";
+//       button1.style.backgroundColor("red");
+//     }
+//     else if (xo ==="x"){
+//       button.textContent = "o";
+//       xo = ""
+//     }
+    
+//   })
+// })
+// function selectXo(){
+// }
+// button1.addEventListener("click",function(){
+//   if (xo === ""){
+//   button1.textContent="x";}
+//   else if(xo === "x");{
+//     button1.textContent = "o";
+//   }
+// })
