364b5c9e1c1e3102a5af7be98d0d78499e0df1c4
abel <abelalebachewasfaw.gmail.com>
2024-07-11 14:39:49 +0300

    Build the game on to run on console

--- script.js
+++ script.js
@@ -1,32 +1,86 @@
-// const container = document.querySelector(".container");
-// const button1= document.getElementById("cell-0");
-// // let displayedContent = "";
-// // displayElement.textContent = displayedContent;
-// let xoState = "waitingForXo1";//initial state
-// let xo;
-// const xoButtons = document.querySelectorAll(".class");
-// xoButtons.forEach((button) =>{
-//   button.addEventListener("click", function(){
-//     if (xo === ""){
-//       displayedContent += button.getAttribute("value");
-//       updateDisplay(displayedContent);
-//       button.textContent="x";
-//       xo = "x";
-//       button1.style.backgroundColor("red");
-//     }
-//     else if (xo ==="x"){
-//       button.textContent = "o";
-//       xo = ""
-//     }
-    
-//   })
-// })
-// function selectXo(){
-// }
-// button1.addEventListener("click",function(){
-//   if (xo === ""){
-//   button1.textContent="x";}
-//   else if(xo === "x");{
-//     button1.textContent = "o";
-//   }
-// })
+//The gameBoard represents the state of the board
+function gameBoard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+  //Create a 2d array that will represent the state of the game board
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board; //getting the entire board
+  const dropToken = (column, player) => {
+    const availableCells = board
+      .filter((row) => row[column].getValue() === 0)
+      .map((row) => row[column]);
+    if (!availableCells.length) return;
+    const lowestRow = availableCells.length - 1;
+    board[lowestRow][column].addToken(player);
+  };
+  // To print our board to the console.
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+  return { getBoard, dropToken, printBoard };
+}
+function Cell() {
+  let value = 0; //Accept a player's token to change the value of the cell
+  const addToken = (player) => {
+    value = player;
+  };
+  const getValue = () => value;
+  return {
+    addToken,
+    getValue,
+  };
+}
+function gameController( // This controls the flow and state of the game's turns, as well as if anybody has won the game.
+  playerOneName = "player One",
+  playerTwoName = "player Two"
+) {
+  const board = gameBoard();
+  const players = [
+    {
+      name: playerOneName,
+      token: 1,
+    },
+    {
+      name: playerTwoName,
+      token: 2,
+    },
+  ];
+  let activePlayer = players[0];
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s switchPlayerTurn.`);
+  };
+  const playRound = (column) => {
+    //drop a token for the current player
+    console.log(
+      `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+    );
+    board.dropToken(column, getActivePlayer().token);
+    //Check to find a winner and and win message
+    //Switch player turn
+    switchPlayerTurn();
+    printNewRound();
+  };
+  printNewRound(); //Initial play game message
+  //For the console version we will only use playRound
+  //For the UI version, getActivePlayer
+  return {
+    playRound,
+    getActivePlayer,
+  };
+}
+const game = gameController();
